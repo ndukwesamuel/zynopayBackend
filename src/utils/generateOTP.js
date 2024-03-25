228058f7a398f37b5747535fe233af9acfdd2677
ndukwesamuel <ndukwesamuel23@yahoo.com>
2024-03-25 14:29:32 +0100

    fix

--- src/utils/generateOTP.js
+++ src/utils/generateOTP.js
@@ -1,7 +1,7 @@
 const otpGenerator = require("otp-generator");
 
 const generateOTP = () => {
-  const otp = otpGenerator.generate(6, {
+  const otp = otpGenerator.generate(4, {
     upperCaseAlphabets: false,
     lowerCaseAlphabets: false,
     specialChars: false,
